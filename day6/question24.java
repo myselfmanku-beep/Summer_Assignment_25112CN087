788c5dfbfb7e743232897b545b28458c9efbe419
myselfmanku-beep <myselfmayankk@gmail.com>
2026-07-05 10:22:22 +0530

    Write a program to Find x^n without pow().

---
+++ day6/question24.java
@@ -0,0 +1,22 @@
+//ques24
+import java.util.Scanner;
+
+public class question24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter power (x): ");
+        int x = sc.nextInt();
+
+        System.out.print("Enter exponent (n): ");
+        int n = sc.nextInt();
+
+        long result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result *= x;
+        }
+
+        System.out.println(x + "^" + n + " = " + result);
+    }
+}
