87b706802eb04078abdd330004761bb84fb5146c
myselfmanku-beep <myselfmayankk@gmail.com>
2026-07-05 10:44:21 +0530

    Write a program to Print reverse number  triangle.

---
+++ day9/question34.java
@@ -0,0 +1,18 @@
+//ques34
+import java.util.Scanner;
+public class question34{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+         int i,j,n;
+         System.out.println("ENTER THE NO OF ROWS IN PATTERN:");
+         n=sc.nextInt();
+         for(i=1;i<=n;i++){
+            for(j=1;j<=n+1-i;j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+         }
+         sc.close();
+    }
+}
