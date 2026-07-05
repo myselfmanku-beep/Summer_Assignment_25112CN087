ebda572dc6e6645508c70ce5503d76ae7313853f
myselfmanku-beep <myselfmayankk@gmail.com>
2026-07-05 10:43:50 +0530

    Write a program to Print reverse star pattern.

---
+++ day9/question33.java
@@ -0,0 +1,18 @@
+//ques33
+import java.util.Scanner;
+public class question33{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+         int i,j,n;
+         System.out.println("ENTER THE NO OF ROWS IN PATTERN:");
+         n=sc.nextInt();
+         for(i=1;i<=n;i++){
+            for(j=1;j<=n+1-i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+         }
+         sc.close();
+    }
+}
